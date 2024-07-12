e9a05dfe05135e8f114394cc50b9c69c73bd9cfb
Amidou KONE <142462945+Amidoukone@users.noreply.github.com>
2024-07-12 09:54:06 +0000

    Folo

---
+++ RoyaleStock-Back/src/main/java/com/groupe_4_ODK/RoyaleStock/RoyaleStockApplication.java
@@ -0,0 +1,13 @@
+package com.groupe_4_ODK.RoyaleStock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoyaleStockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoyaleStockApplication.class, args);
+	}
+
+}
