7640ee5f53398332f5baf1db62c5fead006f9180
Amidou KONE <142462945+Amidoukone@users.noreply.github.com>
2024-07-12 09:54:06 +0000

    Folo

---
+++ RoyaleStock-Back/src/test/java/com/groupe_4_ODK/RoyaleStock/RoyaleStockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.groupe_4_ODK.RoyaleStock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoyaleStockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
