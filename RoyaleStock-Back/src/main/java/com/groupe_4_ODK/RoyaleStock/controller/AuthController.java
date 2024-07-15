e371bb755c2c818a9418a36f37a53987fd946cde
mohamed-sanogo <euromoh68@gmail.com>
2024-07-15 10:07:30 +0000

    bon

--- RoyaleStock-Back/src/main/java/com/groupe_4_ODK/RoyaleStock/controller/AuthController.java
+++ RoyaleStock-Back/src/main/java/com/groupe_4_ODK/RoyaleStock/controller/AuthController.java
@@ -1,6 +1,5 @@
 package com.groupe_4_ODK.RoyaleStock.controller;
 
-import com.groupe_4_ODK.RoyaleStock.entite.Utilisateur;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
