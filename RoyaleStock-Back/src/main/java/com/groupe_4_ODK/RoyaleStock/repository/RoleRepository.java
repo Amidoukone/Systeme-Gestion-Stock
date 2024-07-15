b954da9c14c0b2042341f1a206e51ee028c928b1
mohamed-sanogo <euromoh68@gmail.com>
2024-07-15 08:28:30 +0100

    auth

--- RoyaleStock-Back/src/main/java/com/groupe_4_ODK/RoyaleStock/repository/RoleRepository.java
+++
@@ -1,9 +0,0 @@
-package com.groupe_4_ODK.RoyaleStock.repository;
-
-import com.groupe_4_ODK.RoyaleStock.entite.Role;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface RoleRepository extends JpaRepository<Role, Long> {
-}
