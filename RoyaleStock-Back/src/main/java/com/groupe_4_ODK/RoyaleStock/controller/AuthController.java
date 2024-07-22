17d59d93d514df5ec8848f10f837005cfefe0192
mohamed-sanogo <euromoh68@gmail.com>
2024-07-22 18:01:22 +0000

    back_tmp good

--- RoyaleStock-Back/src/main/java/com/groupe_4_ODK/RoyaleStock/controller/AuthController.java
+++ RoyaleStock-Back/src/main/java/com/groupe_4_ODK/RoyaleStock/controller/AuthController.java
@@ -1,6 +1,8 @@
 package com.groupe_4_ODK.RoyaleStock.controller;
 
 import com.groupe_4_ODK.RoyaleStock.entite.Utilisateur;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
@@ -18,15 +20,15 @@ public class AuthController {
     this.authenticationManager = authenticationManager;
   }
   @PostMapping("/connexion")
-  public String login(@RequestBody Utilisateur loginRequest) {
+  public ResponseEntity<String> login(@RequestBody Utilisateur loginRequest) {
     try {
       Authentication authentication = authenticationManager.authenticate(
         new UsernamePasswordAuthenticationToken(loginRequest.getEmail(), loginRequest.getPassword())
       );
       SecurityContextHolder.getContext().setAuthentication(authentication);
-      return "Utilisateur connecté: " + authentication.getName();
+      return ResponseEntity.ok("Utilisateur connecté: " + authentication.getName());
     } catch (AuthenticationException e) {
-      return "Échec de l'authentification";
+      return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Échec de l'authentification");
     }
   }
 
