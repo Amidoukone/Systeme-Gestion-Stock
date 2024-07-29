35f29ef4c74211b63051c31de564ba8ce230ca87
Atouguesh <diarraatoucarter7655@gamil.com>
2024-07-29 21:07:27 +0000

    Supprimer les répertoires RoyaleStock-Back et RoyaleStock-Front

--- Angular_Front-End/src/app/components/dashboard/dashboard.component.ts
+++ Angular_Front-End/src/app/components/dashboard/dashboard.component.ts
@@ -1,12 +1,204 @@
-import { Component } from '@angular/core';
-
+import Chart from 'chart.js/auto';
+import { ChartModule } from 'primeng/chart';
+import { Component, OnInit } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import * as XLSX from 'xlsx';
+import { saveAs } from 'file-saver';
 @Component({
-  selector: 'app-dashboard',
+  selector: 'app-root',
   standalone: true,
-  imports: [],
+  imports: [RouterOutlet, ChartModule],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit{
+  title = 'Dashboard';
+  public chart: any;
+  options: any;
+  data: any;
 
+  data2: any;
+  options2: any;
+
+  data3: any;
+  options3: any;
+
+  constructor(){}
+
+  ngOnInit() {
+    
+    this.Chart();
+    this.Chart2();
+    this.Chart3();
+    
+
+        
+  }
+
+  Chart(){
+
+    const documentStyle = getComputedStyle(document.documentElement);
+    const textColor = documentStyle.getPropertyValue('--text-color');
+    const textColorSecondary = documentStyle.getPropertyValue('--text-color-secondary');
+    const surfaceBorder = documentStyle.getPropertyValue('--surface-border');
+
+
+      this.data = {
+        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+        datasets: [
+            {
+                label: 'First Achat',
+                data: [65, 59, 80, 81, 56, 55, 40],
+                fill: false,
+                tension: 0.4,
+                borderColor:'blue'
+            },
+            {
+                label: 'Second Dataset',
+                data: [28, 48, 40, 19, 86, 27, 90],
+                fill: false,
+                borderDash: [5, 5],
+                tension: 0.4,
+                borderColor: 'teal'
+            },
+            {
+                label: 'Third Dataset',
+                data: [12, 51, 62, 33, 21, 62, 45],
+                fill: true,
+                borderColor:'orange-500',
+                tension: 0.4,
+                backgroundColor: 'rgba(255,167,38,0.2)'
+            }
+        ]
+    };
+    
+    this.options = {
+        maintainAspectRatio: false,
+        aspectRatio: 0.6,
+        plugins: {
+            legend: {
+                labels: {
+                    color: textColor
+                }
+            }
+        },
+        scales: {
+            x: {
+                ticks: {
+                    color: textColorSecondary
+                },
+                grid: {
+                    color: surfaceBorder
+                }
+            },
+            y: {
+                ticks: {
+                    color: textColorSecondary
+                },
+                grid: {
+                    color: surfaceBorder
+                }
+            }
+        }
+    };
+ 
+  }
+
+  Chart2(){
+    const documentStyle = getComputedStyle(document.documentElement);
+    const textColor = documentStyle.getPropertyValue('--text-color');
+
+        this.data2 = {
+            labels: ['Aliments', 'Vêtements', 'Matériaux'],
+            datasets: [
+                {
+                    data: [300, 100, 50],
+                    backgroundColor: ['blue', 'yellow', 'green'],
+                    hoverBackgroundColor: ['blue', 'yellow', 'green']
+                     
+                }
+            ]
+        };
+
+
+        this.options2 = {
+            cutout: '60%',
+            plugins: {
+                legend: {
+                    labels: {
+                        color: textColor
+                    }
+                }
+            }
+        };
+  }
+  Chart3(){
+    const documentStyle = getComputedStyle(document.documentElement);
+        const textColor = documentStyle.getPropertyValue('--text-color');
+        const textColorSecondary = documentStyle.getPropertyValue('--text-color-secondary');
+        const surfaceBorder = documentStyle.getPropertyValue('--surface-border');
+        
+        this.data3 = {
+            labels: ['Janvier', 'Fevrier', 'Mars', 'Avril', 'Mai', 'Juin', 'Juillet'],
+            datasets: [
+                {
+                    label: 'Bons Entrées',
+                    backgroundColor: documentStyle.getPropertyValue('--blue-500'),
+                    borderColor: documentStyle.getPropertyValue('--blue-500'),
+                    data: [65, 59, 80, 81, 56, 55, 40]
+                },
+                {
+                    label: 'Bons Sorties',
+                    backgroundColor: documentStyle.getPropertyValue('--pink-500'),
+                    borderColor: documentStyle.getPropertyValue('--pink-500'),
+                    data: [28, 48, 40, 19, 86, 27, 90]
+                }
+            ]
+        };
+
+        this.options3 = {
+            maintainAspectRatio: false,
+            aspectRatio: 0.8,
+            plugins: {
+                legend: {
+                    labels: {
+                        color: textColor
+                    }
+                }
+            },
+            scales: {
+                x: {
+                    ticks: {
+                        color: textColorSecondary,
+                        font: {
+                            weight: 500
+                        }
+                    },
+                    grid: {
+                        color: surfaceBorder,
+                        drawBorder: false
+                    }
+                },
+                y: {
+                    ticks: {
+                        color: textColorSecondary
+                    },
+                    grid: {
+                        color: surfaceBorder,
+                        drawBorder: false
+                    }
+                }
+
+            }
+        };
+  }
+
+
+  
 }
+
+
+
+
+
+
