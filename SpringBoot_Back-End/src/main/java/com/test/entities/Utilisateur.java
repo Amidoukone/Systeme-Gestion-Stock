e8bce0c7c4d18a215025666b18358838c253a66c
moussa-goita <mmgoita19@gmail.com>
2024-07-30 16:43:47 +0000

    Bon valider

--- SpringBoot_Back-End/src/main/java/com/test/entities/Utilisateur.java
+++ SpringBoot_Back-End/src/main/java/com/test/entities/Utilisateur.java
@@ -1,16 +1,25 @@
 package com.test.entities;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import java.util.Collection;
+import java.util.Collections;
+
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
-import java.util.Collection;
-import java.util.Collections;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "utilisateurs")
@@ -18,7 +27,6 @@ import java.util.Collections;
 @AllArgsConstructor
 @NoArgsConstructor
 @JsonIgnoreProperties({"authorities"})
-
 public class Utilisateur implements UserDetails {
 
     @Id
