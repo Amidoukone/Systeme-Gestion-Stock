816cc51c09cf34fa08382c4a3632ab69e35ace5e
Sanogo9160 <mahrokiatou9160@gmail.com>
2024-07-31 19:37:13 +0200

    correction des problèmes de local Storage au niveau du login

--- Angular_Front-End/src/app/components/login/login.component.ts
+++ Angular_Front-End/src/app/components/login/login.component.ts
@@ -12,40 +12,49 @@ import {FormsModule} from "@angular/forms";
   styleUrl: './login.component.css'
 })
 export class LoginComponent implements  OnInit{
+
   email: string = '';
   password: string = '';
   isConnected: boolean = false;
   errorMessage: string = '';
   passwordVisible: boolean = false;
 
-
   constructor(public authService: AuthService, private router: Router) {}
 
   ngOnInit() {
-    this.isConnected = !!localStorage.getItem("currentUser");
+    // Vérifie si l'objet localStorage est disponible (environnement navigateur)
+    if (typeof window !== 'undefined' && window.localStorage) {
+      this.isConnected = !!localStorage.getItem("currentUser");
+    } else {
+      console.warn('localStorage is not available.');
+      this.isConnected = false;
+    }
   }
 
   togglePasswordVisibility() {
     this.passwordVisible = !this.passwordVisible;
   }
-  
+
   login() {
     this.authService.login(this.email, this.password).subscribe(
       response => {
         console.log('Login successful', response);
+        // Stocke les informations de l'utilisateur connecté dans le localStorage si disponible
+        if (typeof window !== 'undefined' && window.localStorage) {
+          localStorage.setItem("currentUser", JSON.stringify(response));
+        }
         this.router.navigate(['/dashboard']);
       },
       error => {
-        // console.error('Login failed', error);
         console.error('Connexion échouée🥱', error);
-        this.errorMessage = 'Connexion échouée🥱: ' + (error.error?.message || 'Email or Password invalide');
+        this.errorMessage = 'Connexion échouée🥱: ' + (error.error?.message || 'Email ou mot de passe invalide');
         setTimeout(() => {
           this.errorMessage = '';
         }, 2000);
         this.email = '';
         this.password = '';
-        
       }
     );
-  } 
+  }
+
 }
